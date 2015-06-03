faf1f89318bd19ab42ae39bebcef2d6dae9ecd35
lizkrznarich <e.krznarich@orcid.org>
2015-06-03 15:02:28 -0400

    remove filler text from redirect page

--- oauth-redirect.php
+++ oauth-redirect.php
@@ -111,19 +111,8 @@ if (isset($_GET['code'])) {
 
 <hr>
 
-      <div class="row-fluid marketing">
-        <div class="span12">
-          <h4>Subheading</h4>
-          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
-
-          <!--<h4>Subheading</h4>
-          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
-
-          <h4>Subheading</h4>
-          <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>-->
-        </div>
-
-        <!--<div class="span6">
+      <!--<div class="row-fluid marketing">
+        <div class="span6">
           <h4>Subheading</h4>
           <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
 
@@ -132,8 +121,19 @@ if (isset($_GET['code'])) {
 
           <h4>Subheading</h4>
           <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
-        </div>-->
-      </div>
+        </div>
+
+        <div class="span6">
+          <h4>Subheading</h4>
+          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+          <h4>Subheading</h4>
+          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+
+          <h4>Subheading</h4>
+          <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+        </div>
+      </div>-->
 
       <hr>
 
